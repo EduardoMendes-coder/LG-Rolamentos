368158668d17cb317c56e6c2881dd703dfd039cb
Koda <eduardompereira4@outlook.com>
2022-03-26 21:52:34 -0300

    add role and sex

---
+++ lLgRolamentos/LgRolamentos/backend/classes/model/role.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(deafult=None)
+    
