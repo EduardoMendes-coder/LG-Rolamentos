e1f4ea9edf4e0ea2ef997e46fe8010526d0f1576
Eduardo M <eduardompereira4@outlook.com>
2022-06-03 19:47:31 -0300

    create route in card activities

--- lg-rolamentos/src/components/cards/activities/CardActivities.js
+++ lg-rolamentos/src/components/cards/activities/CardActivities.js
@@ -1,22 +1,31 @@
 import css from "./CardActivities.css";
 import { Card } from "reactstrap";
 import { BiCalendarEdit, BiMedal, BiXCircle } from "react-icons/bi";
+import {Link} from "react-router-dom";
 
 function CardActivities(){
     return(
         <div className="cont">
-            <Card className="act cardFaltas">
-                <h4 className="text textFaltas">FALTAS </h4>
-                <BiCalendarEdit className="iconAct iconBiCal" />
-            </Card>
-            <Card className="act cardMeritos">
-                <h4 className="text textMerits">MÉRITOS</h4>
-                <BiMedal className="iconAct iconMerit"/>
-            </Card>
-            <Card className="act cardAdvertencia">
-                <h4 className="text textAdvertence">ADVERTÊNCIA</h4>
-                <BiXCircle className="iconAct iconAdvert" />
-            </Card>
+            <Link className="butt buttFaltas" to="/">
+                <Card className="act cardFaltas">
+                    <h4 className="text textFaltas">FALTAS </h4>
+                    <BiCalendarEdit className="iconAct iconBiCal" />
+                </Card>
+            </Link>
+
+            <Link className="butt buttMerits" to="/">
+                <Card className="act cardMeritos">
+                    <h4 className="text textMerits">MÉRITOS</h4>
+                    <BiMedal className="iconAct iconMerit"/>
+                </Card>
+            </Link>
+
+            <Link className="butt buttAdvertence" to="/">
+                <Card className="act cardAdvertencia">
+                    <h4 className="text textAdvertence">ADVERTÊNCIA</h4>
+                    <BiXCircle className="iconAct iconAdvert" />
+                </Card>
+            </Link>
         </div>
     );
 }
