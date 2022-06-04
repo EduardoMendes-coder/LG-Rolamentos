3c0f31de13710797906a77c2cfb0cc38c7ea4795
Eduardo M <eduardompereira4@outlook.com>
2022-06-04 05:50:14 -0300

    add cotacao dolar in home page

--- lg-rolamentos/src/components/cards/cotacoes/CardCotacao.js
+++ lg-rolamentos/src/components/cards/cotacoes/CardCotacao.js
@@ -1,18 +1,38 @@
 import { Card, CardBody } from "reactstrap";
 import css from "./CardCotacao.css";
+import axios from 'axios';
 
+let valorCot = []
+
+const getCotacaoDolar = () => {
+    const endpoint = 'http://127.0.0.1:8000/get-dollar-price';
+    const response = axios.get(endpoint)
+        .then((res) => setCotDolar(res.data.cotDolar))
+        .catch((err) => {
+            console.log("Error" + err)
+        })
+}
+
+
+function setCotDolar(dolarAPI){
+    valorCot.push(dolarAPI)
+    localStorage.setItem('valorCot', JSON.stringify(valorCot))
+}
+getCotacaoDolar()
+const newValorDolar = JSON.parse(localStorage.getItem("valorCot"))
 
 function CardCotacao(){
     return(
         <div className="cotacoes">
             <Card className="card cardDolar">
                 <CardBody>
-                    <h5>Cotação Dólar</h5>
+                    <h4>Cotação Dólar </h4>
+                    <h4 className="valDol">{newValorDolar}</h4>
                 </CardBody>
             </Card>
             <Card className="card cardGuarani">
                 <CardBody>
-                    <h5>Cotação Guarani</h5>
+                    <h4>Cotação Guarani</h4>
                 </CardBody>
             </Card>
         </div>
