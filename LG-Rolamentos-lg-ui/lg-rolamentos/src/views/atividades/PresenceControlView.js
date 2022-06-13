3796c98e52898487c17b1161f62eff8efd73a7c3
GabrielLuizC <gabriel10.speed@hotmail.com>
2022-06-12 21:42:19 -0300

    Att faltas

--- LG-Rolamentos-lg-ui/lg-rolamentos/src/views/atividades/PresenceControlView.js
+++ LG-Rolamentos-lg-ui/lg-rolamentos/src/views/atividades/PresenceControlView.js
@@ -1,6 +1,7 @@
 import "./PresenceControlView.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+import { FormText } from "reactstrap";
 
 function PresenceControlView() {
   const employees = JSON.parse(localStorage.getItem("employees"));
@@ -24,7 +25,6 @@ function PresenceControlView() {
 
   return (
     <div className="formPresence">
-      
       <div className="formPresence">
         <h1 className="d-flex justify-content-center">Controle de Presença</h1>
         <form
@@ -38,10 +38,10 @@ function PresenceControlView() {
           }
           method="get"
         >
-          <label className="label">Funcionarios</label>
+          <label className="label mt-4">Funcionarios</label>
           <div className="Pesquisa">
             <select
-              className="form-select"
+              className="form-select w-100"
               onChange={handleEmployeeId}
               name="employee_id"
             >
@@ -55,41 +55,34 @@ function PresenceControlView() {
                   <h6 className="mb-0">{tdata.name}</h6>
                 </option>
               ))}
-              </select>
-          </div>
-          <div>
-              <label className="label">Presença</label>
-              <select className="form-select p-2" name="Presence">
-                <option  name="Presence">
-                  Manhã
-                </option>
-                <option  name="Presence">
-                  Não compareceu
-                </option>
-                <option  name="Presence">
-                  Tarde
-                </option>
             </select>
           </div>
           <div>
-            <label className="label">Descontos</label>
-              <select className="form-select p-2" name="descDia">
-                <option  name="descDia">
-                  Descontar Dia
-                </option>
-                <option  name="descDia">
-                  Não Descontar
-                </option>
-                <option  name="descDia">
-                  Descontar Metade
-                </option>
+            <label className="label mt-3">Presença</label>
+            <select className="form-select w-100 p-2" name="Presence">
+              <option name="Presence">Manhã</option>
+              <option name="Presence">Não compareceu</option>
+              <option name="Presence">Tarde</option>
             </select>
           </div>
           <div>
-            <label className="label">Justificativa</label>
-            <input type="text" name="Justificativa" className="Justificativa"></input>
+            <label className="label mt-3">Descontos</label>
+            <select className="form-select w-100 p-2" name="descDia">
+              <option name="descDia">Descontar Dia</option>
+              <option name="descDia">Não Descontar</option>
+              <option name="descDia">Descontar Metade</option>
+            </select>
           </div>
           <div>
+            <label className="label mt-3">Justificativa</label>
+            <textarea
+              type="text"
+              name="Justificativa"
+              className="Justificativa"
+              maxlength="150"
+            ></textarea>
+          </div>
+          <div className="d-flex flex-column">
             <input
               onChange={handleFromDate}
               className="dt_Presenca"
@@ -100,11 +93,16 @@ function PresenceControlView() {
               data-date-format="DD MMMM YYYY"
               placeholder="data"
             ></input>
+            <FormText className="ms-1 mb-1">
+              Data a ser registrada como falta
+            </FormText>
           </div>
 
-
-        
-          <input className="btnPresence" type="submit" value="Pesquisar"></input>
+          <input
+            className="btnPresence mb-5 p-3"
+            type="submit"
+            value="Cadastrar Falta"
+          ></input>
         </form>
       </div>
     </div>
